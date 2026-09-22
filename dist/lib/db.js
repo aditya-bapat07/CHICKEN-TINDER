ae4550f7ba1ea1ae1b25e1fb9f75d9b470558646
Aditya Bapat <aditya_bapat@icloud.com>
2026-09-22 19:02:46 +0530

    cleaning

--- dist/lib/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const client_1 = require("@prisma/client");
-exports.db = new client_1.PrismaClient(process.env.TEST_DATABASE_URL
-    ? { datasources: { db: { url: process.env.TEST_DATABASE_URL } } }
-    : undefined);
